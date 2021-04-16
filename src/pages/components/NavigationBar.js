66e6f95feb63676aa1f85937477bb061ff0a8bfd
Neelesh Sharma <dh207709@dunnhumby.com>
2021-04-16 18:03:40 +0530

    Fs

--- src/pages/components/NavigationBar.js
+++ src/pages/components/NavigationBar.js
@@ -1,28 +1,70 @@
-import {Row, Col, Button} from 'antd';
+import {Row, Col, Button, Modal} from 'antd';
 import './NavigationBar.css'
 import {NavLink} from 'react-router-dom';
+import { useState } from 'react';
 
 export default function NavigationBar(props){
+
+    const [modalVisible, setModalVisible] = useState(false);
+
     return (
         <Row id="navbar" style={{backgroundColor:props.bgColor}}> 
             <Col span={2}></Col>
             <Col span={3}> 
+                    <NavLink to="/">
                     <img
                     style={{width:'50px'}}
                     src="https://cdn.zeplin.io/6061ed0fe3392716f0cc504a/assets/708D202C-EFBC-4A32-94AF-1FBC448FFFDB.svg"/>
-               
+               </NavLink>
             </Col>
             <Col span={17} align="right">
-                <NavLink to="/">
+                <NavLink className="showInDesktop" to="/">
                 <span style={{ borderBottom: props.currentComponent == "work" ? '2px solid #15204e' : '0px', marginRight:'40px', fontSize:'16px', color:'#796b6d', position:'absolute',right:'50px', top:'5px'}}>
                     Work 
                 </span>
                 </NavLink>
-                <NavLink to="/about">
+                <NavLink className="showInDesktop" to="/about">
                 <span style={{ borderBottom: props.currentComponent == "about" ? '2px solid #15204e' : '0px', fontSize:'16px', color:'#796b6d', position:'absolute', right:'14px', top:'5px'}} >
                      About 
                 </span>
                 </NavLink>
+                <img onClick={()=>{
+                    setModalVisible(true);
+                }} className="showInMobile" style={{display:'none', position:'absolute',right:'10px', top:'10px'}} src="https://cdn.zeplin.io/6061ed0fe3392716f0cc504a/assets/D2F9915A-087B-4C1E-8479-80714FB56369.svg"/>
+                <Modal
+                
+          visible={modalVisible}
+          title=""
+          onOk={()=>{
+
+          }} 
+          onCancel={()=>{
+            setModalVisible(false);
+          }}
+          footer={[]}
+        >
+            <div style={{height:'50vh', paddingTop:'60px', paddingLeft:'30px'}}>
+            <NavLink 
+            onClick={()=>{
+            setModalVisible(false);
+          }}
+           to="/">
+                <h2 style={{fontFamily:'Palatino-Roman'}} >
+                     Home
+                </h2>
+                </NavLink>
+                <br/> 
+         <NavLink 
+         onClick={()=>{
+            setModalVisible(false);
+          }}
+         to="/about">
+                <h2 style={{fontFamily:'Palatino-Roman'}} >
+                     About 
+                </h2>
+                </NavLink>
+            </div>
+        </Modal>
             </Col>
             <Col span={2}></Col>
 
--- src/pages/home/components/ProjectsContainer.js
+++ src/pages/home/components/ProjectsContainer.js
@@ -1,18 +1,28 @@
 import './ProjectsContainer.css';
 import {NavLink} from 'react-router-dom';
-import {Row, Col} from 'antd';
+import {Row, Col, Card} from 'antd';
 import group29 from './../../../assets/group-29.svg';
 import group19 from './../../../assets/group-19.svg';
 import group30 from './../../../assets/group-30.svg';
 import group31 from './../../../assets/group-31.png';
 import group32 from './../../../assets/group-32.jpg';
 import group33 from './../../../assets/group-33.svg';
+import rapidoBack from './../../../assets/rapidoBack.svg';
+import selfiBack from './../../../assets/selfiBack.svg';
+import aidBack from './../../../assets/aidBack.svg';
+import vingBack from './../../../assets/vingBack.svg';
+import innazBack from './../../../assets/innazBack.svg';
+import brandingBack from './../../../assets/brandingBack.svg';
+import { useState } from 'react';
+
+import ReactCardFlip from 'react-card-flip'
 
 const projects = [
     {
         id:1,
         title: 'Rapido’s Booking Experience',
         image: group19,
+        flip: rapidoBack,
         isPrivate: false,
         route:"/rapido"
     },
@@ -20,6 +30,7 @@ const projects = [
         id:2,
         title: 'Breast- Self Examination App',
         image: group29,
+        flip: selfiBack,
         isPrivate: true,
         route:"/selfi"
     },
@@ -27,6 +38,7 @@ const projects = [
         id:3,
         title: 'Website Redesign ',
         image: group30,
+        flip: aidBack,
         isPrivate: false,
         route:"/aid"
     },
@@ -34,6 +46,7 @@ const projects = [
         id:4,
         title: 'Meeting Room Platform',
         image: group31,
+        flip: vingBack,
         isPrivate: true,
         route:"/ving"
     },
@@ -41,6 +54,7 @@ const projects = [
         id:5,
         title: 'Event Website',
         image: group32,
+        flip: innazBack,
         isPrivate: false,
         route:"/innaz"
     },
@@ -48,82 +62,132 @@ const projects = [
         id:6,
         title: 'Branding + Illustrations',
         image: group33,
+        flip: brandingBack,
         isPrivate: null,
         route:"/branding_illustrations"
     },
 ]
 
-export function ProjectCard(props){
-    return (
-        
-        <div  
-        align="center" 
-        className="projectCard"
-        style={{  
-            position:'relative', 
-            marginBottom:'30px',  
-            }}> 
-            <img className="projectCardImg" align="center" src={props.image} style={{width:'70%'}}/>
-               
-            <br/><br/>
-        </div>
-    )
-}
 
 export default function ProjectsContainer(){
+
+    let inImg = false;
+ 
+    const [zero, setZero] = useState(false);
+    const [one, setOne] = useState(false);
+    const [two, setTwo] = useState(false);
+    const [three, setThree] = useState(false);
+    const [four, setFour] = useState(false);
+    const [five, setFive] = useState(false);
+
+ 
     return (
         
         <div id="projectsContainer">
             <Row>
                 <Col xs={{span:24}} lg={{span:12}} align="center">
                 <NavLink to={projects[0].route}>
-                <ProjectCard image={projects[0].image}
-                    title={projects[0].title}
-                    isPrivate={projects[0].isPrivate}
-                    route={projects[0].route}
-            />
+                <ReactCardFlip isFlipped={zero} flipDirection="horizontal">  
+            <img
+             onMouseEnter={()=>{  
+                setZero(true) ;
+            }}
+             className="projectCardImg"   align="center"  src={projects[0].image}  style={{width:'65%', marginBottom:'30px'}}/>
+            <img
+            onMouseOut={()=>{
+                setZero(false);
+            }}  
+             className="projectCardImg" align="center" style={{width:'65%', marginBottom:'30px'}} src={projects[0].flip}/> 
+      </ReactCardFlip>
+      <br/><br/>
             </NavLink>
                 </Col>
                 <Col xs={{span:24}} lg={{span:12}} align="center">
                 <NavLink to={projects[1].route}>
-                <ProjectCard image={projects[1].image}
-                    title={projects[1].title}
-                    isPrivate={projects[1].isPrivate}
-                    route={projects[1].route}
-            />
+                <ReactCardFlip isFlipped={one} flipDirection="horizontal">  
+            <img
+             onMouseEnter={()=>{  
+                setOne(true) ;
+            }}
+             className="projectCardImg"   align="center"  src={projects[1].image}  style={{width:'65%', marginBottom:'30px'}}/>
+            <img
+            onMouseOut={()=>{
+                setOne(false);
+            }}  
+             className="projectCardImg" align="center" style={{width:'65%', marginBottom:'30px'}} src={projects[1].flip}/> 
+      </ReactCardFlip>
+      <br/><br/>
             </NavLink>
 
                 </Col><Col xs={{span:24}} lg={{span:12}} align="center">
                 <NavLink to={projects[2].route}>
-                <ProjectCard image={projects[2].image}
-                    title={projects[2].title}
-                    isPrivate={projects[2].isPrivate}
-                    route={projects[2].route}
-            />
+                <ReactCardFlip isFlipped={two} flipDirection="horizontal">  
+            <img
+             onMouseEnter={()=>{  
+                setTwo(true) ;
+            }}
+             className="projectCardImg"   align="center"  src={projects[2].image}  style={{width:'65%', marginBottom:'30px'}}/>
+            <img
+            onMouseOut={()=>{
+                setTwo(false);
+            }}  
+             className="projectCardImg" align="center" style={{width:'65%', marginBottom:'30px'}} src={projects[2].flip}/> 
+      </ReactCardFlip>
+      <br/><br/>
             </NavLink>
-                </Col><Col xs={{span:24}} lg={{span:12}} align="center">
-                <NavLink to={projects[3].route}>
-                <ProjectCard image={projects[3].image}
-                    title={projects[3].title}
-                    isPrivate={projects[3].isPrivate}
-                    route={projects[3].route}
-            />
+                </Col>
+                
+            <Col xs={{span:24}} lg={{span:12}} align="center">
+            <NavLink to={projects[3].route}>
+                <ReactCardFlip isFlipped={three} flipDirection="horizontal">  
+            <img
+             onMouseEnter={()=>{  
+                setThree(true) ;
+            }}
+             className="projectCardImg"   align="center"  src={projects[3].image}  style={{width:'65%', marginBottom:'30px'}}/>
+            <img
+            onMouseOut={()=>{
+                setThree(false);
+            }}  
+             className="projectCardImg" align="center" style={{width:'65%', marginBottom:'30px'}} src={projects[3].flip}/> 
+      </ReactCardFlip>
+      <br/><br/>
             </NavLink>
-                </Col><Col xs={{span:24}} lg={{span:12}} align="center">
+                </Col>
+            
+            <Col xs={{span:24}} lg={{span:12}} align="center">
                 <NavLink to={projects[4].route}>
-                <ProjectCard image={projects[4].image}
-                    title={projects[4].title}
-                    isPrivate={projects[4].isPrivate}
-                    route={projects[4].route}
-            />
+                <ReactCardFlip isFlipped={four} flipDirection="horizontal">  
+            <img
+             onMouseEnter={()=>{  
+                setFour(true) ;
+            }}
+             className="projectCardImg"   align="center"  src={projects[4].image}  style={{width:'65%', marginBottom:'30px'}}/>
+            <img
+            onMouseOut={()=>{
+                setFour(false);
+            }}  
+             className="projectCardImg" align="center" style={{width:'65%', marginBottom:'30px'}} src={projects[4].flip}/> 
+      </ReactCardFlip>
+      <br/><br/>
             </NavLink>
-                </Col><Col xs={{span:24}} lg={{span:12}} align="center">
+                </Col>
+            
+            <Col xs={{span:24}} lg={{span:12}} align="center">
                 <NavLink to={projects[5].route}>
-                <ProjectCard image={projects[5].image}
-                    title={projects[5].title}
-                    isPrivate={projects[5].isPrivate}
-                    route={projects[5].route}
-            />
+                <ReactCardFlip  isFlipped={five} flipDirection="horizontal">  
+            <img
+             onMouseEnter={()=>{  
+                setFive(true) ;
+            }}
+             className="projectCardImg"   align="center"  src={projects[5].image}  style={{width:'65%', marginBottom:'30px'}}/>
+            <img
+            onMouseOut={()=>{
+                setFive(false);
+            }}  
+             className="projectCardImg" align="center" style={{width:'65%', marginBottom:'30px'}} src={projects[5].flip}/> 
+      </ReactCardFlip>
+      <br/><br/>
             </NavLink>
                 </Col>
             </Row>
