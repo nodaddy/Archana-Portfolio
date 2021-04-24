034d0f9bc8bd386399f56d544b8d49f323a38d16
Neelesh Sharma <dh207709@dunnhumby.com>
2021-04-25 02:36:50 +0530

    Dfasa

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,10 +6,10 @@ import ProjectsContainer from './components/ProjectsContainer'
 export default function Home(){
     return (
       <div id="home" align="center"> 
-          <div className="home-title" style={{color: '#796b6d', fontFamily:'PalatinoETW01-Roman', fontSize:'50px', marginBottom:'15px'}}>
+          <div className="home-title" style={{color: '#796b6d', fontFamily:'Palatino-Roman', fontSize:'50px', marginBottom:'15px'}}>
               Designing through the night.
          </div>
-         <div className="home-title" style={{marginTop:'-30px',color: '#796b6d', fontFamily:'PalatinoETW01-Roman', fontSize:'50px', marginBottom:'18px'}}>
+         <div className="home-title" style={{marginTop:'-30px',color: '#796b6d', fontFamily:'Palatino-Roman', fontSize:'50px', marginBottom:'18px'}}>
               to bring sane ideas to the light.
          </div>
          <div id="home-about" style={{fontFamily:'Raleway', fontSize:'20px'}}>Hello there! I am Archana and I love creating memorable visual design experiences 
